51dcdc14764e459c18d60f269562132c91944545
Aleksandr Solovyov <solovyov-alexander@yandex.ru>
2022-10-10 16:34:56 +0300

    Initial commit: tools set up and begining
    - Set up GULP and its plugins.
    - Create index.html file.

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var sass = require('sass');
+// PostCSS and its plugins:
+var postcss = require('postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+// Web server and HTTP
+var webserver = require('gulp-webserver');
+var http = require('http');
+
+gulp.task('css', function() {
+  var plugins = [
+    // Using .browserslistrc file.
+    // Otherwise change browsers list here.
+    //autoprefixer({browsers: ['last 1 version']}),
+    autoprefixer(),
+    cssnano()
+  ];
+  return gulp.src('./scss/*.scss')
+    .pipe(sass({
+               errorLogToConsole: true,
+               outputStyle: 'nested'     // to turn on compression here, set 'compress'
+    }))
+    .pipe(postcss(plugins))
+    .pipe(rename({suffix: '.min'}))
+  //.on('error', console.error.bind(console)) // error output to console
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('server', function(done) {
+  return gulp.src('dist')
+    .pipe(webserver({
+      livereload: false,
+      directoryListing: false,
+      open: true,
+      port: 8000,
+      middleware: function(req, res, next) {
+        if (/_kill_\/?/.test(req.url)) {
+          res.end();
+          stream.emit('kill');
+        }
+        next();
+      }
+  }));
+});
+
+gulp.task('server-stop', function(done) {
+  http.request('http://localhost:8000/_kill_')
+    .on('close', done)
+    .end();
+});
