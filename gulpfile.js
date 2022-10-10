70203a7b59a21559b4c0da2596f112b3425df230
Aleksandr Solovyov <solovyov-alexander@yandex.ru>
2022-10-10 20:13:59 +0300

    Fix multiple errors in css gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var sass = require('sass');
+var sass = require('gulp-sass')(require('sass'));
 // PostCSS and its plugins:
-var postcss = require('postcss');
+var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 // Web server and HTTP
@@ -13,7 +13,7 @@ var http = require('http');
 gulp.task('css', function() {
   var plugins = [
     // Using .browserslistrc file.
-    // Otherwise change browsers list here.
+    // Otherwise change browsers list here:
     //autoprefixer({browsers: ['last 1 version']}),
     autoprefixer(),
     cssnano()
@@ -21,11 +21,11 @@ gulp.task('css', function() {
   return gulp.src('./scss/*.scss')
     .pipe(sass({
                errorLogToConsole: true,
-               outputStyle: 'nested'     // to turn on compression here, set 'compress'
+               //outputStyle: 'compress'     // uncomment to turn on compression
     }))
     .pipe(postcss(plugins))
     .pipe(rename({suffix: '.min'}))
-  //.on('error', console.error.bind(console)) // error output to console
+    //.on('error', console.error.bind(console)) // error output to console
     .pipe(gulp.dest('./dist/css/'));
 });
 
