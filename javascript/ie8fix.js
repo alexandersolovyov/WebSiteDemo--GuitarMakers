e72686f368117542a7143e18e0c7f16a1ec0d741
Aleksandr Solovyov <solovyov-alexander@yandex.ru>
2022-10-19 19:22:23 +0300

    Add HTML5 fixes for IE8

---
+++ javascript/ie8fix.js
@@ -0,0 +1,12 @@
+
+(function() {
+  document.createElement('header');
+  document.createElement('nav');
+  document.createElement('section');
+  document.createElement('article');
+  document.createElement('aside');
+  document.createElement('footer');
+  document.createElement('button');
+  var el = document.getElementById('header');
+  el.setAttribute('style', 'display:none;');
+}());
