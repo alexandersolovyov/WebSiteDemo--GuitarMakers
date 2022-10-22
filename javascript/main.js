718a0255323fcf1f7bce2350aa26aea2fe361388
Aleksandr Solovyov <solovyov-alexander@yandex.ru>
2022-10-22 14:04:48 +0300

    Add highlighting main menu items and more
    - Add functions to manipulate with CSS classes.
    - Add all needed for menu item highilghting on window scroll.
    - Dont use addEventListeners() from now.

--- javascript/main.js
+++ javascript/main.js
@@ -11,6 +11,23 @@ window.site = (function () {
      * Элементы и действия при старте
      *====================================
      */
+    // Идентификаторы элементов для навигации.
+    // -Порядок в этом списке важен! Он повторяет порядок якорей на странице.
+    // -Если это ID блока - в конце есть '-section'
+    // -Если нужно ID ссылки в меню - окончание нужно заменить на '-ref'
+    // (в названиях переменных anchor = цель ссылки, ref = ссылка в меню)
+    var anchorIds = [
+      'services-section',
+      'demo-section',
+      'features-section',
+      'prices-section',
+      'form-section',
+      'contacts-section',
+    ];
+
+    // Рассчитать расположение элементов для навигации
+    var anchorPositions = window.utils.getYOffsets(anchorIds);
+
     // Захватываем элементы меню
     var menuBtn = document.getElementById('nav-menu__button');
     var menu = document.getElementById('nav-menu');
@@ -19,6 +36,62 @@ window.site = (function () {
     // Убрать появление меню по Hover
     menu.className = menu.className.replace('is-nav-menu-hover', '');
 
+    /*========================================
+     * Функции для вставки в обработку событий
+     *========================================
+     */
+    // Для события onscroll, выполняет функцию только если прошло указанное
+    // время. Возвращает функцию, которая должна выполняться при обработке
+    // события.
+    var throttle = function (func, time_ms) {
+      var locked = false; // глобальная переменная
+      return function() {
+        if (locked) { return; } // если функция func ещё не запускалась
+        locked = true;
+        setTimeout(function() { // через определённое время
+          func();               // запускаем полезную функцию
+          locked = false;       // и разблокируем 
+        }, time_ms);
+      };
+    };
+    // Подсветка пунктов главного меню при прокрутке страницы
+    var highlightMainRef = function() {
+      // Функция для выбора ссылки по ИД целевого раздела
+      var get_ref = function (section_id) {
+        return document.getElementById(section_id.replace('-section', '-ref'));
+      };
+      // Класс которым помечаем пункт меню
+      var marker_class = 'is-nav-menu__link-active';
+      // Берём позицию окна
+      var win_pos = window.utils.getWindowOffset();
+      // Начинаем с нижнего раздела-цели.
+      // Если низ экрана ниже его верха - подсвечиваем пункт меню.
+      var is_marked = false; // станет true если один элемент помечен
+      var i = anchorIds.length - 1; // итератор списка якорей
+      var ref_el = get_ref(anchorIds[i]); // нижний элемент страницы
+      if (win_pos.bottom > anchorPositions[anchorIds[i]]) {
+        window.utils.addClass(ref_el, marker_class);
+        is_marked = true; // помечен, остальные якоря только очищаем от метки
+      }else {
+        window.utils.removeClass(ref_el, marker_class);
+      }
+      // Проход по остальным элементам.
+      i -= 1;
+      while (i >= 0) {
+        ref_el = get_ref(anchorIds[i]);
+        if (is_marked) { // уже помечено, дальше только убираем
+          window.utils.removeClass(ref_el, marker_class);
+        // Если верх элемента выше верха окна - подсвечиваем пункт меню.
+        }else if (win_pos.top > (anchorPositions[anchorIds[i]] - 15)) {
+          window.utils.addClass(ref_el, marker_class);
+          is_marked = true;
+        }else { // если ещё не дошли до совпадения
+          window.utils.removeClass(ref_el, marker_class);
+        }
+        i -= 1;
+      }
+      console.log('Highlight end');
+    };
     /*====================================
      * Слушаем события
      *====================================
@@ -36,13 +109,19 @@ window.site = (function () {
     // При клике на списке:
     // -если список виден (is-nav-menu__list-visible) - прячем его;
     // -меняем иконку обратно на "меню"
-    window.utils.addEventListener(navList, 'click', function () {
-      navList.className = navList.className.replace('is-nav-menu__list-visible', '');
+    navList.onclick = function () {
+      window.utils.removeClass(navList, 'is-nav-menu__list-visible');
       if (window.utils.getRealDisplay(menuBtn) !== 'none') { // если кнопка видна
         navIcon.className = navIcon.className.replace('icon-cancel', 'icon-menu');
       }
-    });
+    };
+    // При изменении размеров окна рассчитать расположение элементов-целей для
+    // навигации
+    window.onresize = function () {
+      anchorPositions = window.utils.getYOffsets(anchorIds);
+    };
     // При скролле подсвечиваем пункт меню
+    window.onscroll = throttle(highlightMainRef, 100);
   };
     
   return site;
--- javascript/utils.js
+++ javascript/utils.js
@@ -24,8 +24,11 @@ window.utils = (function () {
     }
   };
   /*
-   * Reliable function for adding event listeners.
+   * Function for adding event listeners.
    * (Works also on IE8)
+   * NOTE: Better dont use it. It is only for reference as it can misuse
+   * browser's addEventListener() function.
+   * Better use window.onscroll, element.onclick etc.
    * @param elem - DOM element to listen.
    * @param type - String - name of an event without 'on' prefix.
    * @param listener - listener function, accepts no parameters for now.
@@ -40,30 +43,151 @@ window.utils = (function () {
       elem.attachEvent("onclick", listener);
     }
   };
+
   /*
-   * Toggles class of given element with multiple classes.
+   * Adds CSS class to given element with no, single or multiple classes.
+   * @param elem - DOM element to change.
+   * @param classNam - String - name of class to add.
+   * @return true if class added, false if there already was such class.
+   */
+  utils.addClass = function (elem, classNam) {
+    var ret = false;
+    if (elem.className.length) {
+      var classes = elem.className.split(' ');
+      var found = false;
+      for (var i in classes) {
+        if (classes[i] === classNam) {
+          found = true;
+          break;
+        }
+      }
+      if (! found) {
+        elem.className += ' ' + classNam;
+        ret = true;
+      }
+    }else {
+      elem.className = classNam;
+      ret = true;
+    }
+    return ret;
+  };
+  /*
+   * Removes CSS class of given element with no, single or multiple classes.
+   * @param elem - DOM element to change.
+   * @param classNam - String - name of class to add.
+   * @return true if class removed, false if there was no such class.
+   */
+  utils.removeClass = function (elem, classNam) {
+    var ret = false;
+    if (! elem.className.length) { // if empty
+      return false;
+    }
+    var classes = elem.className.split(' ');
+    new_class = '';
+    for (var i in classes) {
+      if (classes[i] !== classNam) {
+        new_class = new_class + classes[i] + ' ';
+      }else { // filtered
+        ret = true;
+      }
+    }
+    if (ret) { // if filtered = removed
+      // trim last space
+      if (new_class.length) {
+        new_class = new_class.slice(0, (new_class.length - 1));
+      }
+      elem.className = new_class;
+    }
+    return ret;
+  };
+  /*
+   * Toggles CSS class of given element with no, single or multiple classes.
    * @param elem - DOM element to change.
    * @param classNam - String - name of class to toggle.
    * @return true if class set, false if class removed.
    */
-    utils.toggleClass = function (elem, classNam) {
-    var i;
-    var classes = elem.className.split(' ');
-    var new_class = '';
-    ret = false;
-    if (elem.className.indexOf(classNam) < 0) { // add class
-      new_class = elem.className;
-      new_class = new_class + ' ' + classNam;
-      ret = true;
-    }else { // remove class
-      for (i in classes) {
-        if (classes[i] !== classNam) {
+  utils.toggleClass = function (elem, classNam) {
+    var ret = true;
+    if (elem.className.length) {
+      var classes = elem.className.split(' ');
+      var new_class = '';
+      var found = false;
+      for (var i in classes) {
+        if (classes[i] !== classNam) { // filter out
           new_class = new_class + classes[i] + ' ';
+        }else { // found - removed
+          found = true;
         }
       }
+      if (! found) { // not found - need to add
+        elem.className += ' ' + classNam;
+      }else { // found = filtered, get filter result
+        if (new_class.length) {
+          // trim last space added in cycle
+          new_class = new_class.slice(0, (new_class.length - 1));
+        }
+        elem.className = new_class;
+        ret = false;
+      }
+    }else {
+      elem.className = classNam;
     }
-    elem.className = new_class;
     return ret;
+  };
+
+  /*
+   * Calculates position in pixels of given element in a document.
+   * @param element - element for which to calculate.
+   * @return object with two number elements: offsetLeft, offsetTop.
+   */
+  utils.getOffsetPosition = function (element) {
+    var offsetLeft = 0, offsetTop = 0;
+    do {
+      offsetLeft += element.offsetLeft;
+      offsetTop += element.offsetTop;
+      element = element.offsetParent;
+    } while (element);
+    return {'offsetLeft': offsetLeft, 'offsetTop': offsetTop};
+  };
+  /*
+   * Calculates positions of top point of given elements on the page.
+   * @param elementIds - An array of element IDs to calculate their positions.
+   * @return object with element ID strings as keys and offset number
+   * in px as values.
+   */
+  utils.getYOffsets  = function(elementIds) {
+    var ret = {};
+    var el;
+    for (var el_id in elementIds) {
+      el = document.getElementById(elementIds[el_id]);
+      ret[elementIds[el_id]] = utils.getOffsetPosition(el).offsetTop;
+    }
+    return ret;
+  };
+  /*
+   * Get current window scroll position.
+   * @return object of two elements: 'top' and 'bottom', containing numbers -
+   * positions of window top and bottom respectively.
+   */
+  utils.getWindowOffset = function () {
+    var pos = {};
+    pos.top = 0;
+    pos.bottom = 0;
+    if (window.pageYOffset !== undefined) {
+      pos.top = window.pageYOffset;
+    }else if (document.documentElement.scrollTop) {
+      pos.top = document.documentElement.scrollTop;
+    }else if (document.body.parentNode.scrollTop) {
+      pos.top = document.body.parentNode.scrollTop;
+    }else if (document.body.scrollTop) {
+      pos.top = document.body.scrollTop;
+    }
+    if (window.innerHeight) {
+      pos.bottom = pos.top + window.innerHeight;
+    }else {
+      pos.bottom = pos.top + document.documentElement.clientHeight;
+    }
+    return pos;
   };
 
   return utils;
